43dd20cce07e0268deb1d590c190f01de9116319
Julian Schmidt <julian.schmidt@adesso-health.de>
2022-05-17 15:41:31 +0200

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { Service } from './app.services';
-import ArrayStore from 'devextreme/data/array_store';
 import CustomStore from 'devextreme/data/custom_store';
 import DataSource from 'devextreme/data/data_source';
 import { HttpClient } from '@angular/common/http';
+import { of } from 'rxjs';
+import { LoadOptions } from 'devextreme/data/load_options';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'Angular';
@@ -16,19 +17,42 @@ export class AppComponent {
   public tagBoxDataSource: any = {};
 
   constructor(readonly service: Service, readonly httpClient: HttpClient) {
-    this.tagBoxDataSource = new DataSource({
-      store: new CustomStore({
-          key: "ID",
-          loadMode: "raw",
-          load: () => {
-              return httpClient.get('http://localhost:3100/products').toPromise();
-          }
-      })
-  });
+    // this.tagBoxDataSource = new DataSource({
+    //   store: new CustomStore({
+    //     loadMode: 'raw',
+    //     byKey: () => {
+    //       return of().toPromise();
+    //     },
+    //     load: (options: LoadOptions) => {
+    //       const { searchValue } = options;
+    //       return httpClient
+    //         .get(`http://localhost:3100/products?search${searchValue}`)
+    //         .toPromise();
+    //     },
+    //   }),
+    // });
+    this.tagBoxDataSource = new CustomStore({
+      key: 'id',
+      byKey: () => {
+        return of().toPromise();
+      },
+      load: (options: LoadOptions) => {
+        const { searchValue } = options;
+        console.log('options', options);
+        return httpClient
+          .get(`http://localhost:3100/products?search=${searchValue}`)
+          .toPromise();
+      },
+    });
   }
 
   onCustomItemCreating(args) {
     const newValue = args.text;
     args.customItem = newValue;
+  }
+
+  public search(searchEvent): void {
+    console.log('searchEvent', searchEvent);
+    // this.onStateChange(this.internalGridState || { selectedRowKeys: [] });
   }
 }
